c66a42d7cc2e3a3679300b22282b3856467b32e6
Sambuka <vova.kurtiak@gmail.com>
2022-08-25 15:21:09 +0200

    graph temp wind add

--- src/features/currentWeather/chunks/TemperatureGraph.js
+++ src/features/currentWeather/chunks/TemperatureGraph.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from "react-redux"
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { LabelList, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 import '../../../index.css'
 
@@ -9,36 +9,49 @@ import { selectCurrentWeather } from '../currentWeather-slice';
 const TemperatureGraph = () => {
 
     const {status, error, forecast} = useSelector(selectCurrentWeather)
-    console.log(forecast.forecastday[0].hour);
     
     let graphData = forecast?.forecastday?.reduce((acc, item) => {
         return [...item.hour.map((hour) => (
-          {time : hour.time, temp : hour.temp_c}))]
+          {
+            time : hour.time, 
+            temperature : hour.temp_c, 
+            feels : hour.feelslike_c,
+            wind_s : hour.wind_kph,
+            wind_dir : hour.wind_dir,
+            humidity: hour.humidity, 
+          }
+        ))]
     }, [])
+
+    const CustomTooltip = ({ active, payload, label }) => {
+      console.log(payload);
+      if (active && payload && payload.length) {
+        return (
+          <div className="custom-tooltip">
+            {payload.map(item => (
+              <p style={{color: item.color}}>{`${item.name} : ${item.value} ${item.unit}`}</p>
+            ))}
+          </div>
+        );
+      }
+    
+      return null;
+    };
 
   return (
     <>
     {status === 'loading' && <h2>Loading...</h2>}
     {(status === 'received' && !error) && (
-        <ResponsiveContainer width='100%' height={300}>
-            <AreaChart data={graphData}
+        <ResponsiveContainer width='100%' height={350}>
+            <LineChart data={graphData}
                 margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-                <defs>
-                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-                <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
-                </linearGradient>
-                <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
-                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
-                </linearGradient>
-                </defs>
                 <XAxis dataKey="time" />
-                <YAxis />
-                <CartesianGrid strokeDasharray="3 3" />
-                <Tooltip />
-                <Area type="monotone" dataKey="temp" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-            </AreaChart>
+                <YAxis padding={{ top: 10 }} />
+                <Tooltip content = {<CustomTooltip />}/>
+                <Line type="monotone" name={'Temperature'} unit={'°C'} dataKey="temperature" stroke="#757761" />
+                <Line type="monotone" name={'Feels like'} unit={'°C'} dataKey="feels" stroke="#f4e76e" />
+                <Line type="monotone" name={'Wind speed'} unit={'km/h'} dataKey="wind_s" stroke="#8ff7a7" />
+            </LineChart>
         </ResponsiveContainer>
         )}
     </>
